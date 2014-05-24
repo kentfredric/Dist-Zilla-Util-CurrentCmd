3edc2e908287cf179a7463411f839c31d2e946f2
Kent Fredric <kentfredric@gmail.com>
2014-05-25 10:41:58 +1200

    Build results of 16b9fdb (on master)

--- lib/Dist/Zilla/Util/CurrentCmd.pm
+++ lib/Dist/Zilla/Util/CurrentCmd.pm
@@ -51,20 +51,6 @@ use Sub::Exporter '-setup' => { exports => [qw( current_cmd is_build is_install 
 
 
 
-sub _is_cmd {
-  my ($want_cmd) = @_;
-  my $i = 0;
-  while ( my @frame = caller($i) ) {
-    $i++;
-    *STDERR->printf( "%s\n", $frame[3] );
-    next unless $frame[3] =~ /\ADist::Zilla::App::Command::(.*)::([^:\s]+)\z/msx;
-    return $1 eq $want_cmd;
-    next unless "Dist::Zilla::App::Command::${want_cmd}::execute" eq $frame[3];
-    return 1;
-  }
-  return;
-}
-
 
 
 
@@ -145,9 +131,9 @@ version 0.001000
 
 =head1 DESCRIPTION
 
-This module exists in case you are absolutely certain you want to have different behaviours for either a plugin, or a bundle, to trigger on ( or off ) a specific phase.
+This module exists in case you are absolutely certain you want to have different behaviors for either a plugin, or a bundle, to trigger on ( or off ) a specific phase.
 
-Usually, this is a bad idea, and the need to do this suggests a poor choice of workflow to begin with.
+Usually, this is a bad idea, and the need to do this suggests a poor choice of work-flow to begin with.
 
 That said, this utility is I<probably> more useful in a bundle than in a plugin, in that it will be slightly more optimal than say, having an C<ENV> flag to control this difference.
 
@@ -168,17 +154,17 @@ For instance:
 
 Convenience shorthand for C<current_cmd() eq 'build'>
 
-=head2 C<is_build>
+=head2 C<is_install>
 
-Convenience shorthand for C<current_cmd() eq 'build'>
+Convenience shorthand for C<current_cmd() eq 'install'>
 
 =head1 CAVEATS
 
-User beware, this code is both hacky and new, and relies on using C<caller> to determine which C<Dist::Zilla::App::Command::> we are running under.
+User beware, this code is both hackish and new, and relies on using C<caller> to determine which C<Dist::Zilla::App::Command::> we are running under.
 
 There may be conditions that there are no C<Command>s in the C<caller> stack which meet this definition, or the I<first> such thing may be a misleading representation of what is actually running.
 
-And there's a degree of uncertainty of reliability, because I haven't yet divised reliable ways of testing it that don't involve invoking dzil ( which is problematic on testers where Dist::Zilla is in C<@INC> but C<dzil> is not in C<ENV{PATH}> )
+And there's a degree of uncertainty of reliability, because I haven't yet devised reliable ways of testing it that don't involve invoking C<dzil> ( which is problematic on testers where C<Dist::Zilla> is in C<@INC> but C<dzil> is not in C<ENV{PATH}> )
 
 To that extent, I don't even know for sure if this module works yet, or if it works in a bundle, or if it works in all commands, or if it works under C<Dist::Zilla::App::Tester> as expected.
 
